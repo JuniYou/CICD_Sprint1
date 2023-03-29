90bd5645583979d3eb2290a6a7ffc01af76dc802
Shaked Asido <shakedasido@gmail.com>
2023-03-29 15:55:34 +0300

    Added page and style dictionaries- for css and html files

--- src/index.js
+++ src/index.js
@@ -2,11 +2,31 @@ import express from "express";
 
 const port = process.env.PORT || 80;
 const app = express();
+
+
  app.get('/', (req, res)=>{
-    console.log("get function 23");
-    res.send("<h1 style='color: red;'>Hello from 2 shaked</h1>");
+
+    res.sendFile('page/index.html', { root: "./"})
  }
  );
+
+ app.get('/Login', (req, res)=>{
+
+    res.sendFile('page/login.html', { root: "./"})
+ }
+ );
+
+ app.get('/getstyle', (req, res)=>{
+
+   res.sendFile('style/style.css', { root: "./"})
+}
+);
+
+app.get('/getscripts', (req, res)=>{
+
+   res.sendFile('scripts/scripts.js', { root: "./"})
+}
+); 
 
  app.listen(port, ()=>{
     console.log("listen to port " + port);
