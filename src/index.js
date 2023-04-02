a9239472293bb70a47a8966fde6ee3a276927d73
Shaked Asido <shakedasido@gmail.com>
2023-04-02 12:18:05 +0300

    added linter to the project

--- src/index.js
+++ src/index.js
@@ -1,33 +1,24 @@
-import express from "express";
+import express from 'express';
 
 const port = process.env.PORT || 80;
 const app = express();
 
+app.get('/', (req, res) => {
+   res.sendFile('page/index.html', { root: './' });
+});
 
- app.get('/', (req, res)=>{
+app.get('/Login', (req, res) => {
+   res.sendFile('page/login.html', { root: './' });
+});
 
-    res.sendFile('page/index.html', { root: "./"})
- }
- );
+app.get('/getstyle', (req, res) => {
+   res.sendFile('style/style.css', { root: './' });
+});
 
- app.get('/Login', (req, res)=>{
+app.get('/getscripts', (req, res) => {
+   res.sendFile('scripts/scripts.js', { root: './' });
+});
 
-    res.sendFile('page/login.html', { root: "./"})
- }
- );
-
- app.get('/getstyle', (req, res)=>{
-
-   res.sendFile('style/style.css', { root: "./"})
-}
-);
-
-app.get('/getscripts', (req, res)=>{
-
-   res.sendFile('scripts/scripts.js', { root: "./"})
-}
-); 
-
- app.listen(port, ()=>{
-    console.log("listen to port " + port);
- })
+app.listen(port, () => {
+   console.log(`listen to port ${port}`);
+});
