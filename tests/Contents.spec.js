237bffbc53c08a72220c55f5bbdc1982b2381bfb
elibenaharon <eli14741@gmail.com>
2023-04-03 18:14:19 +0300

    Finished first test: comparison of two strings that represent the info about our web

--- tests/Contents.spec.js
+++ tests/Contents.spec.js
@@ -11,9 +11,22 @@ describe('Contents', () => {
                 response.on('data', (chunk) => {
                     data = data + chunk.toString();
                 });
+            
                 response.on('end', () => {
                     const body = data;
                     ind = data.indexOf('We are a social network designated for juniors. Those who want to develop their skills and become the next seniors. Our goal is to help you make this dream come true!<br /><br />How? With us, you can easily access the most current job inquiries, job offers, and links to free and relevant study sites categorized by subject. And above all, you will be able to express yourself and create new friendships and opportunities! <br /><br />So, what are you waiting for? Sign up for free!');
                 });
+            })
+            
+            request.on('error', (error) => {
+                console.log('An error', error);
+            });
+            
+            request.end();
+            await new Promise(resolve => setTimeout(resolve, 50));
+            expect(ind).not.to.be.eql(-1);
+        })
+    })
+})
 
 
