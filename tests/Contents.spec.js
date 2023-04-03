49751dfa7bf5db77a47733fece64b6ff752534bd
michakl1 <michakl1@ac.sce.ac.il>
2023-04-03 16:57:39 +0300

    Started first test (main page test)

--- tests/Contents.spec.js
+++ tests/Contents.spec.js
@@ -1,4 +1,13 @@
 import {expect} from "chai";
 import * as https from "http";
 
-
+describe('Contents', () => {
+    describe('Main page', () =>{
+        it('Site information', async () =>{
+            var ind = -1;
+            const url = 'http://localhost';
+            const request = https.request(url, (response) => {
+                let data = '';
+                response.on('data', (chunk) => {
+                    data = data + chunk.toString();
+                });
